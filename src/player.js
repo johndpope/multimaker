1f9bd7efb5eddfc1138bf4ec0dae8a9c88f1eb1d
Bradley K. Harms <brad.k.harms@gmail.com>
2018-01-19 19:49:34 -0600

    reorganization of files and code

---
+++ src/player.js
@@ -0,0 +1,44 @@
+const Phaser = require('phaser/dist/phaser');
+
+module.exports = class Player {
+  /**
+   * @param {Phaser.Scene} scene 
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(scene, x, y) {
+    this.scene = scene;
+    this.jumpPower = 0;
+    this.jumpPowerInc = 10;
+    this.jumpPowerMax = 200;
+    this.sprite = scene.physics.add.sprite(x, y, 'charles');
+    this.sprite.body.setBounce(0);
+    this.cursors = scene.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.sprite.body.onFloor()) {
+      this.sprite.body.setVelocityX(0);
+      if (this.cursors.left.isDown) {
+        this.sprite.body.setVelocityX(-100);
+      } else if (this.cursors.right.isDown) {
+        this.sprite.body.setVelocityX(100);
+      }
+      if ((this.cursors.space.isDown || this.cursors.up.isDown)) {
+        this.jumpPower = Math.min(
+          this.jumpPower + this.jumpPowerInc,
+          this.jumpPowerMax
+        );
+      } else {
+        this.sprite.body.setVelocityY(-this.jumpPower);
+        this.jumpPower = 0;
+      }
+    }
+    if (Math.abs(this.sprite.body.velocity.x) < 1) {
+      this.sprite.x = Math.round(this.sprite.x);
+    }
+    if (Math.abs(this.sprite.body.velocity.y) < 1) {
+      this.sprite.y = Math.round(this.sprite.y);
+    }
+  }
+}
--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,6 @@
 const Phaser = require('phaser/dist/phaser');
+const Player = require('./player');
+
 new Phaser.Game({
   
   type: Phaser.WEBGL,
@@ -16,67 +18,34 @@ new Phaser.Game({
   scene: new class extends Phaser.Scene {
     preload() {
       window.scene = this;
-      const R_AS = '../assets/antispace';
+      const AS = '../projects/antispace';
       
-      const R_AS_SP = `${R_AS}/sprites`;
-      this.load.atlas('charles', `${R_AS_SP}/charles.png`, `${R_AS_SP}/charles.json`);
+      const AS_SP = `${AS}/sprites`;
+      this.load.atlas('charles', `${AS_SP}/charles.png`, `${AS_SP}/charles.json`);
       
-      const R_AS_TS = `${R_AS}/tiles`;
-      this.load.image('tileset00', `${R_AS_TS}/tsNAZ.png`);
-
-      // --- //
-
-      const P_AS = '../projects/antispace';
+      const AS_TS = `${AS}/tiles`;
+      this.load.image('tileset00', `${AS_TS}/tsNAZ.png`);
       
-      const P_AS_M = `${P_AS}/maps`;
-      this.load.tilemapJSON('room000', `${P_AS_M}/room000.json`);
+      const AS_M = `${AS}/maps`;
+      this.load.tilemapJSON('room000', `${AS_M}/room000.json`);
     }
 
     create() {
-      this.player = this.physics.add.sprite(100, 50, 'charles');
-      this.player.body.setBounce(0);
+      this.player = new Player(this, 100, 50);
       
       this.tm = this.add.tilemap('room000');
       this.ts = this.tm.addTilesetImage('tileset00');
       this.layer = this.tm.createStaticLayer(0, this.ts, 0, 0);
       this.layer.setCollisionBetween(64,128);
       
-      this.physics.add.collider(this.player, this.layer);
+      this.physics.add.collider(this.player.sprite, this.layer);
       
       this.cameras.main.setBounds(0, 0, this.tm.widthInPixels, this.tm.heightInPixels);
-      this.cameras.main.startFollow(this.player);
-
-      this.cursors = this.input.keyboard.createCursorKeys();
-      this.jumpPower = 0;
-      this.jumpPowerInc = 10;
-      this.jumpPowerMax = 200;
+      this.cameras.main.startFollow(this.player.sprite);
     }
 
     update() {
-      if (this.player.body.onFloor()) {
-        this.player.body.setVelocityX(0);
-        if (this.cursors.left.isDown) {
-          this.player.body.setVelocityX(-100);
-        } else if (this.cursors.right.isDown) {
-          this.player.body.setVelocityX(100);
-        }
-        if ((this.cursors.space.isDown || this.cursors.up.isDown)) {
-          //this.player.body.setVelocityY(-150);
-          this.jumpPower = Math.min(
-            this.jumpPower + this.jumpPowerInc,
-            this.jumpPowerMax
-          );
-        } else {
-          this.player.body.setVelocityY(-this.jumpPower);
-          this.jumpPower = 0;
-        }
-      }
-      if (Math.abs(this.player.body.velocity.x) < 1) {
-        this.player.x = Math.round(this.player.x);
-      }
-      if (Math.abs(this.player.body.velocity.y) < 1) {
-        this.player.y = Math.round(this.player.y);
-      }
+      this.player.update();
     }
   }
 });
